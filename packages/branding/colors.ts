5028bbec045d19c72795ec830807c343c4aae54a
aaronkchsu <aaronkchsu@gmail.com>
2019-11-18 09:05:39 -0700

    fix: update starling color

--- packages/branding/colors.ts
+++ packages/branding/colors.ts
@@ -2,7 +2,7 @@ const Colors = {
     ibisRed: '#FE295C', // rgba(254,41,92, 1)
     waxWing: '#F3F3F3', // rgba(243,243,243, 1)
     notBlack: '#21000C', // rgba(23,13,17, 1)
-    starling: '#391881', // rgba(57,24,129, 1)
+    starling: '#3507B4', // rgba(53,7,180, 1)
     seafoam: '#0FEBC5', // rgba(15,235,197, 1)
     buntingBlue: '#27AAFF', // rgba(39,170,255, 1)
     popNYellow: '#FFD400', // rgba(255,212,0, 1)
